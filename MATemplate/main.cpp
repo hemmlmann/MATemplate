feb1ee175e2fb8c5e6054ef642e13f54f6c1cabd
hemmlmann <hemmlmann@outlook.com>
2014-08-22 17:51:16 +0200

    fixed #1 | first working version of the template! yay :D

--- MATemplate/main.cpp
+++ MATemplate/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include "matrix.h"
+
+//a little demo <(^_°)>
 
 int main() {
 	
+	Matrix<int> lolMatrix(4, 3);
+	lolMatrix.rand();
+	std::cout << "Rows: " << lolMatrix.rows() << " | Columns: " << lolMatrix.cols() << std::endl;
+	lolMatrix.print();
 
+	getchar();
+
+	Matrix<int> roflMatrix = lolMatrix.trans();
+	std::cout << "Rows: " << roflMatrix.rows() << " | Columns: " << roflMatrix.cols() << std::endl;
+	roflMatrix.print();
+
+	getchar();
 
 	return 42;
 }
--- MATemplate/matrix.h
+++ MATemplate/matrix.h
@@ -2,78 +2,154 @@
 #define MATRIX_H
 
 #include <vector>
-#include <iostream>
 
-template<typename T>
+//------------------------- M a t r i x -------------------------
+template <typename T>
 class Matrix {
 public:
+	//Some constructor(s)...
+	Matrix(int N, int M)
+	{
+		for (int i = 0; i < N; i++){ content.push_back(std::vector<T>(M)); }
+		m_rows = N;
+		m_cols = M;
+	}
 
-	Matrix() : M{ 0 }, N{ 0 }, items{ 0 };
-
-	Matrix operator+(const Matrix& op2Mat) {
-		if (this->rows() == op2Mat.rows() && this->cols() == op2Mat.cols()) {
-			Matrix<T> retMat;	//create return matrix
-			for (int i = 0; i < N*M; i++) {
-				retMat.items.push_back(this->items.at(i) += op2Mat.items.at(i));
+	//Some operators...
+	Matrix operator+(const Matrix& rhs) {
+		if (this->rows() == rhs.rows() && this->cols() == rhs.cols()) {
+			Matrix<T> retMat(rows(), cols());
+			for (unsigned int i = 0; i < rows(); i++) {
+				for (unsigned int j = 0; j < cols(); j++) {
+					retMat.set(i, j, (this->at(i, j) + rhs.at(i, j)));
+				}
 			}
 			return retMat;
 		}
-		else {
-			std::cout << "Could not add... Matrix dimension mismatch!\nError Code: :(" << std::endl;
-		}
+		else { throw Matrix_Dimension_Mismatch(); std::cout << "Could not add... Matrix dimension mismatch!\nError Code: :(" << std::endl; }
 	}
-	Matrix operator-(const Matrix& op2Mat) {
-		if (this->rows() == op2Mat.rows() && this->cols() == op2Mat.cols()) {
-			Matrix<T> retMat;
-			for (int i = 0; i < N*M; i++) {
-				retMat.items.push_back(this->items.at(i) -= op2Mat.items.at(i));
+	Matrix operator-(const Matrix& rhs) {
+		if (this->rows() == rhs.rows() && this->cols() == rhs.cols()) {
+			Matrix<T> retMat(rows(), cols());
+			for (unsigned int i = 0; i < rows(); i++) {
+				for (unsigned int j = 0; j < cols(); j++) {
+					retMat.set(i, j, (this->at(i, j) - rhs.at(i, j)));
+				}
 			}
 			return retMat;
 		}
-		else {
-			std::cout << "Could not subtract... Matrix dimension mismatch!\nError Code: :/" << std::endl;
+		else { throw Matrix_Dimension_Mismatch(); std::cout << "Could not subtract... Matrix dimension mismatch!\nError Code: :(" << std::endl; }
+	}
+	Matrix operator*(const Matrix& rhs) {
+		if (this->cols() == rhs.rows()) {
+			Matrix<T> retMat(this->rows(), rhs.cols());
+			for (unsigned int i = 0; i < retMat.rows(); i++) {
+				for (unsigned int j = 0; j < retMat.cols(); j++) {
+					T sum{};	//(!) C++11 - always initializes variable to default value. which is nice...
+					for (int k = 0; k < retMat.rows(); k++) {
+						sum += this->at(i, k) * rhs.at(k, j);
+					}
+					retMat.set(i, j, sum);
+				}
+			}
+			return retMat;
+		}
+		else { throw Matrix_Dimension_Mismatch(); std::cout << "Could not multiply... Matrix dimension mismatch!\nError Code: D:" << std::endl; }
+	}
+	Matrix operator==(const Matrix& rhs) {	//returns a boolean matrix containing ones at indices with equal value
+		if (this->rows() == rhs.rows() && this->cols() == rhs.cols()) {
+			Matrix<T> retMat(rows(), cols());
+			for (unsigned int i = 0; i < rows(); i++) {
+				for (unsigned int j = 0; j < cols(); j++) {
+					if (this->at(i, j) == rhs.at(i, j)) { retMat.set(i, j, 1); }
+					else { retMat.set(i, j, 0); }
+				}
+			}
+			return retMat;
+		}
+		else { throw Matrix_Dimension_Mismatch(); std::cout << "Could not compare... Matrix dimension mismatch!\nError Code: :/" << std::endl; }
+	}
+
+
+	//Some functions...
+	void zero() {	//"zeros out" the matrix
+		for (unsigned int i = 0; i < rows(); i++) {
+			for (unsigned int j = 0; j < cols(); j++) { content[i][j] = 0; }
 		}
 	}
-	Matrix operator*(const Matrix& op2Mat){
-		if (this->cols() == op2Mat.rows()) {
-			Matrix<T> retMat;
-			for (int i = 0; i < N*M; i++) {
-				retMat.items.push_back([&]{for (int k = 0; k < this->cols(); k++){ this->at(i*cols() + k)*(op2Mat.at(i + k*op2Mat.cols()) }}));
+
+	void rand() {	//fills (as of yet) the matrix with ascending values per row - will be randomized at some point!
+		for (unsigned int i = 0; i < rows(); i++) {
+			for (unsigned int j = 0; j < cols(); j++) { content[i][j] = i; }
+		}
+	}
+
+	Matrix id() {	//returns the identity matrix for any given square matrix
+		if (this->rows() == this->cols()) {
+			Matrix<T> retMat(rows(), cols());
+			for (unsigned int i = 0; i < rows(); i++) {
+				for (unsigned int j = 0; j < cols(); j++) {
+					if (i == j) { retMat.set(i, j, 1); }
+					else { retMat.set(i, j, 0); }
+				}
+			}
+			return retMat;
+		}
+		else { throw Matrix_Not_Square();  std::cout << "Matrix is not square!\nError Code: :O" << std::endl; }
+	}
+
+	Matrix trans() {	//returns the transposed matrix
+		Matrix<T> retMat(cols(), rows());
+		for (unsigned int i = 0; i < retMat.rows(); i++) {
+			for (unsigned int j = 0; j < retMat.cols(); j++) {
+				retMat.set(i, j, (this->at(j, i)));
 			}
 		}
-		else {
-			std::cout << "Could not multpily... Matrix dimension mismatch!\nError Code: D:" << std::endl;
+		return retMat;
+	}
+
+	void print() {	//prints a matrix most trivially...atm...
+		for (unsigned int i = 0; i < rows(); i++) {
+			for (unsigned int j = 0; j < cols(); j++) {
+				std::cout << content[i][j] << "  ";
+			}
+			std::cout << "\n";
 		}
+		std::cout << std::endl;
 	}
 
-	//returns a boolean matrix. contains ones at indices with equal value
-	Matrix operator==(const Matrix& op2Mat){
-
+	T at(unsigned int n, unsigned int m) const
+	{
+		if (n < rows() && m < cols()) { return content[n][m]; }
+		else { throw std::out_of_range("out of range"); }
 	}
 
-	void trans() {
-
+	void set(unsigned int n, unsigned int m, T val) {
+		if (n < rows() && m < cols()) { content[n][m] = val; }
+		else { throw std::out_of_range("out of range");}
 	}
 
-	void inv() {
-
-	}
-
-	void const display(){
-
-	}
-	T const at(int i){ return items.at(i); }
-
-	void setRows(int rows) { M = rows; }
-	void setCols(int cols) { N = cols; }
-
-	int const rows(){ return M; }
-	int const cols(){ return N; }
+	unsigned int rows() const { return m_rows; }
+	unsigned int cols() const { return m_cols; }
 
 private:
-	int M; //# of rows
-	int N; //# of cols
-	std::vector<T> items;
+	unsigned int m_rows;
+	unsigned int m_cols;
+	std::vector<std::vector<T>> content;
+};
+
+
+
+//----------------- E x c e p t i o n   H a n d l i n g -----------------
+//not implemented yet!
+
+struct Matrix_Dimension_Mismatch
+{
+	Matrix_Dimension_Mismatch(){};
+};
+struct Matrix_Not_Square
+{
+	Matrix_Not_Square(){};
 };
 
 #endif //MATRIX_H
